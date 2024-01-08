7a2ff7ccd81a167fbf9e894f53c01274a4d8f1ee
IT21378270 <it21378270@my.sliit.lk>
2024-01-08 17:08:26 +0530

    register, login api created in auth.js

--- api/index.js
+++ api/index.js
@@ -1,13 +1,21 @@
 const express = require('express');
-const app = express();
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
 
-mongoose.connect(
-    "mongodb+srv://banuka1216:banuka1216@cluster0.60i7kst.mongodb.net/shop?retryWrites=true&w=majority"
-    )
+const app = express();
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL)
     .then(() => console.log("DB connection successfull"))
     .catch((err) => console.log(err));
 
-app.listen(5000, () => {
+app.use(express.json());
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+
+app.listen(process.env.PORT || 5000, () => {
     console.log('Backend server is running');
 })
