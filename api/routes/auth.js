d1d45b230f9e91c25b5f7683f584107e47a5096a
IT21378270 <it21378270@my.sliit.lk>
2024-01-08 17:16:33 +0530

    jsonwebtoken added

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,13 +1,14 @@
 const router = require('express').Router();
 const User = require("../models/User")
 const CryptoJS = require("crypto-js");
+const jwt = require('jsonwebtoken');
 
 // REGISTER
 router.post('/register', async (req, res) => {
     const newUser = new User({
         username: req.body.username,
         email: req.body.email,
-        password: CryptoJS.AES.encrypt(req.body.password, process.env.SECRET_KEY).toString(),
+        password: CryptoJS.AES.encrypt(req.body.password, process.env.PASS_KEY).toString(),
     })
 
     newUser.save()
@@ -23,14 +24,21 @@ router.post('/login', async (req, res) => {
         });
         !user && res.status(401).json("Wrong password or username!");
 
-        const hashedPassword = CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY);
+        const hashedPassword = CryptoJS.AES.decrypt(user.password, process.env.PASS_KEY);
         const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
 
         originalPassword !== req.body.password && res.status(401).json("Wrong password or username!");
 
+        const accessToken = jwt.sign({
+            id: user._id,
+            isAdmin: user.isAdmin
+        }, process.env.JWT_KEY, {
+            expiresIn: "5d"
+        })
+
         const { password, ...others } = user._doc;
 
-        res.status(200).json(others);
+        res.status(200).json({...others, accessToken});
     } catch (err) {
         res.status(500).json(err);
     } 
